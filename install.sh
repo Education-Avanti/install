74c097b222e76d66a0350e9d9351753ed0d3c57a
scainet7 <>
2024-12-27 16:29:17 +0300

    V0.1

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+# Убедитесь, что передается ссылка на репозиторий
+if [ -z "https://github.com/Education-Avanti/dop_c.git" ]; then
+  echo "Ошибка: не указана ссылка на репозиторий."
+  exit 1
+fi
+
+# Скачивание репозитория в директорию Desktop
+echo "Скачиваю репозиторий с ссылки https://github.com/Education-Avanti/dop_c.git"
+cd /home/admin/Desktop
+git clone https://github.com/Education-Avanti/dop_c.git
+
+# Переход в директорию репозитория
+cd /home/admin/Desktop/dop_c || { echo "Ошибка: не удалось зайти в директорию dop_c"; exit 1; }
+
+if [ -z "git clone https://github.com/WiringPi/WiringPi.git" ]; then
+  echo "Ошибка: не указана ссылка на репозиторий."
+  exit 1
+fi
+
+echo "Скачиваю репозиторий с ссылки git clone git clone https://github.com/WiringPi/WiringPi.git"
+git clone https://github.com/WiringPi/WiringPi.git
+
+# Выполнение устоновки
+echo "Запуск установки библиотеки WiringPi"
+cd WiringPi
+./build
+cd ../
+
+echo "Запуск установки дополнительных библиотек..."
+make || { echo "Ошибка: make не удалось выполнить."; exit 1; }
+
+# Установка с правами суперпользователя
+echo "Запуск sudo make install..."
+sudo make install || { echo "Ошибка: не удалось выполнить sudo make install."; exit 1; }
+
+# Удаление лишних файлов
+echo "Удаляю лишние файлы..."
+rm -rf WiringPi/ include/ src/ Makefile libdopLibc.* VERSION || { echo "Ошибка: не удалось выполнить удаление файлов"; exit 1; }
+
+# Скрипт завершен
+echo "Установка завершена!"
